9c3bfc535c8a12341670f9d953a6ee88ba766b4b
sofia tofigh <sst2140@columbia.edu>
2020-04-02 19:14:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |number, element|
+  array.each_with_index do |element, number|
     puts "#{number}. #{element}"
   
   end
