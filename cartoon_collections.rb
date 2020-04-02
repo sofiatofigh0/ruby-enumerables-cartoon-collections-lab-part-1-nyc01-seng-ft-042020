a669f837508b074382de4c786a4a76ae90b3be92
sofia tofigh <sst2140@columbia.edu>
2020-04-02 19:10:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |number, element|
-    phrase = puts "#{number+1}. #{element}"
-    phrase
+    puts "#{number+1}. #{element}"
+  
   end
   # Use `each_with_index` to enumerate over the provided array
   #
