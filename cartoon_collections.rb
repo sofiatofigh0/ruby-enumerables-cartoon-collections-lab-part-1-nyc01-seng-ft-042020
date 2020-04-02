ec18a509c2eb14707571b6ec9ed907061197a1d2
sofia tofigh <sst2140@columbia.edu>
2020-04-02 19:13:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |number, element|
-    puts "#{number+1}. #{element}"
+    puts "#{number}. #{element}"
   
   end
   # Use `each_with_index` to enumerate over the provided array
